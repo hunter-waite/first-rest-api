13e695cede1a139473603765665cdde330a0fac7
Hunter Waite <hunterwaite@Hunters-MacBook-Air.local>
2021-04-06 15:33:09 -0700

    Added search function to get request

--- sample_backend.py
+++ sample_backend.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 users = { 
@@ -32,10 +34,19 @@ users = {
    ]
 }
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
 @app.route('/users')
 def get_users():
+   search_username = request.args.get('name') #accessing the value of parameter 'name'
+   if search_username :
+      subdict = {'users_list' : []}
+      for user in users['users_list']:
+         if user['name'] == search_username:
+            subdict['users_list'].append(user)
+      return subdict
    return users
